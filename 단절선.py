d427b7c5ca63a08cff71f9ec1d17bd466c39ca20
ymg5218 <threebr123@naver.com>
2022-12-20 22:34:20 +0900

    2022/12/20

---
+++ 단절선.py
@@ -0,0 +1,68 @@
+#11400
+import sys
+sys.setrecursionlimit(10**6)
+input = sys.stdin.readline
+def dfs(start, parent):
+    global cnt
+    cnt += 1
+    visited[start] = True # 방문한 노드는 True로 분류
+    order[start] = cnt # 방문 순서 기입
+    lsv = order[start] # 주변 최소 순번 변수 lsv
+
+    for childNode in graph[start] : # 해당 노드와 연결된 자식노드들 탐색
+        if childNode == parent : # 부모노드로 돌아가는 것은 무시한다
+            continue
+        
+        if visited[childNode] == True : # 이미 방문한 상태라면
+            lsv = min(lsv, order[childNode]) # 방문했던 노드를 주변 최소 순번으로 설정하고 진행
+            continue
+        
+        subtree = dfs(childNode, start) # 서브트리도 dfs 탐색
+        lsv = min(subtree, lsv) # 주변 최소 순번이 서브트리의 부모 노드와 주변 최소 순번 노드 중, 작은 값으로 설정
+
+        # 현재 간선을 제외하고 우회로가 없다면 : 브릿지
+        """판별 방법"""
+        # 서브트리의 dfs 반환 값(주변 최소 순번)이 부모 노드의 순번보다 크다 : 브릿지
+        if subtree > order[start] :
+            bridge.append([start,childNode])
+    
+    return lsv # 주변 최소 순번을 return
+
+
+if __name__ == "__main__":
+    V,E = map(int, input().split())
+
+    graph = [[]for _ in range(0,V+1)] # 그래프 틀 선언
+    # 크기가 노드의 개수 + 1인 이유 : 0번째 인덱스는 더미로 둘 것이기 때문.
+
+    visited = [False] * (V+1) # dfs를 위한 방문여부 확인 bool 리스트
+    # 크기가 노드의 개수 + 1인 이유 : 0번째 인덱스는 더미로 둘 것이기 때문.
+
+    order = [-1] * (V+1) # 순회를 체크하기 위한 배열 선언. -1로 초기화
+    # 크기가 노드의 개수 + 1인 이유 : 0번째 인덱스는 더미로 둘 것이기 때문.
+
+
+    for i in range(0,E):
+        v1, v2 = map(int,input().split())
+        # 그래프에 각 노드의 연결성 삽입
+        graph[v1].append(v2)
+        graph[v2].append(v1)
+    
+    #graph를 출력하면 인접리스트의 형태로 저장되었음을 알 수 있다.
+    
+    bridge = [] # 브릿지를 담을 배열 선언
+
+    cnt = 0 # 노드 방문 순서를 나타내는 변수
+    
+    dfs(1, None)
+    
+    # 모든 간선의 양 끝점은 [작은수 노드, 큰 수 노드] 로 통일시키기 위해 정렬을 진행
+    # 이는 간선의 양 끝점을 입력받아 브릿지 여부 판단할 때, 데이터의 정렬을 통일시켜 브릿지인지 판단하기 용이하기 위함.
+    for i in range(0,len(bridge)):
+        bridge[i].sort()
+    
+    print(len(bridge))
+    if len(bridge) > 0:
+        for line in bridge:
+            print(line)
+    
---
+++ 지름길.py
@@ -0,0 +1,55 @@
+# 1446
+
+# 모든 지름길은 일방통행 : 단방향 간선
+
+# 길이 D의 고속도로에 N개의 지름길 존재
+
+# 반복문을 통해 진행하니 복잡도가 너무 높았음 = > 해결책 : 힙을 활용한 구현
+# 참고 - heapq : https://littlefoxdiary.tistory.com/3
+# 참고 - 힙 구조를 이용한 다익스트라 : https://velog.io/@tks7205/%EB%8B%A4%EC%9D%B5%EC%8A%A4%ED%8A%B8%EB%9D%BC-%EC%95%8C%EA%B3%A0%EB%A6%AC%EC%A6%98-with-python
+
+
+import heapq
+
+def dijkstra(start):
+    heap = []
+    heapq.heappush(heap,(0,start)) # 힙 빌드업
+    distance[start] = 0
+
+    while heap: # 
+        print(heap)
+        dis, now = heapq.heappop(heap) # 우선순위가 가장 낮은 값 = 가장 작은 거리가 나온다.
+
+        if distance[now] < dis :
+            continue # 이미 최솟값이 distance 배열의 now번째 인덱스에 저장돼있다면 무시
+
+        for element in road[now]:
+            length = dis + element[1] # 배열의 각 요소 1번째 인덱스는 거리. 현재 dis와 더하여 length에 저장
+
+            if distance[element[0]] > length: # 만약 length가 이미 저장된 총 거리보다 작다면
+                distance[element[0]] = length # 최솟값으로 치환
+                heapq.heappush(heap,(length,element[0]))
+    
+
+
+
+
+if __name__ == "__main__":
+    INF = 1e8
+    N,D = map(int,input().split()) # 지름길 개수, 고속도로 길이 입력
+    road = [[]for _ in range(0,D+1)] # 거리 1 마다 노드가 있다고 판단함
+    distance = [INF] * (D+1) # 거리
+
+    for i in range(0,D): # 모든 노드는 거리 1로 초기화
+        road[i].append((i+1,1))
+    
+    for i in range(0,N):
+        start, end, cost = map(int,input().split())
+        if end <= D: # 지름길의 끝점이 고속도로 최종 목적지를 넘어가면 무효
+            road[start].append((end,cost))
+        
+
+    dijkstra(0)\
+    print(distance[D])
+    
+        
