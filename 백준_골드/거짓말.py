4864bef557be9575b0b918e3d84a2fcc307f56c1
Yeom MG <threebr123@naver.com>
2024-04-30 10:31:57 +0900

    Python Study
    24/04/30

---
+++ 백준_골드/거짓말.py
@@ -0,0 +1,29 @@
+# Union-Find 알고리즘 숙지하고 풀기
+
+# 1043
+import sys
+from collections import deque
+
+input = sys.stdin.readline  
+
+def solution():
+    cnt = 0
+
+    
+
+    print(cnt)
+
+
+if __name__ == "__main__":
+    N, M = map(int, input().split())
+    
+    known = set(input().split()[1:])
+
+    # 0번째 인덱스는 더미 값
+    parties = []
+
+    for _ in range(M):
+        party = set(input().split()[1:])
+    
+    
+    solution()
---
+++ 백준_브론즈/N 찍기.py
@@ -0,0 +1,6 @@
+# 2741
+
+N = int(input())
+
+for i in range(N):
+    print(i + 1)
