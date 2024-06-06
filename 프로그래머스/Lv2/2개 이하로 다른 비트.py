8783b2904b9060e3cb89b0b127224319703c2f17
Yeom MG <threebr123@naver.com>
2024-06-06 20:36:57 +0900

    Python Study
    24/06/06

---
+++ 프로그래머스/Lv2/2개 이하로 다른 비트.py
@@ -0,0 +1,40 @@
+def binary_bit(num):
+    result = ""
+    length = 0
+    stack = []
+    while num > 0:
+        stack.append(str(num % 2))
+        num //= 2
+        length += 1
+    
+    while stack:
+        result += stack.pop()
+
+    return result, length
+
+def solution(numbers):
+    answer = []
+
+    for idx in range(len(numbers)):
+        now = numbers[idx]
+        now_binary, now_length = binary_bit(now)
+        next = now + 1
+        while True:
+            diff = 0
+            next_binary, next_length = binary_bit(next)
+            if now_length != next_length:
+                diff += 1
+            for i in range(now_length):
+                if now_binary[i] != next_binary[i]:
+                    diff += 1
+                if diff > 2:
+                    break
+            if diff <= 2:
+                answer.append(next)
+            next += 1
+
+    return answer
+
+if __name__ == "__main__":
+    numbers = [2, 7]
+    print(solution(numbers))
