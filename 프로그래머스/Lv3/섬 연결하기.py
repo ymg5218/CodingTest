5e566464d45725aacbb47e1dcc4a151f89a7df19
Yeom MG <threebr123@naver.com>
2024-06-14 01:17:16 +0900

    Python Study
    24/06/14

---
+++ 프로그래머스/Lv3/섬 연결하기.py
@@ -0,0 +1,12 @@
+def solution(n ,costs):
+    answer = 0
+
+
+
+    return answer
+
+
+if __name__ == "__main__":
+    n = 4
+    costs = [[0,1,1],[0,2,2],[1,2,5],[1,3,1],[2,3,8]]
+    print(solution())
