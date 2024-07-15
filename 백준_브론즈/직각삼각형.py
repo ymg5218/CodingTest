149a66d9b496ab4c2116a5e70d774d24a298c600
Yeom MG <threebr123@naver.com>
2024-07-16 01:48:37 +0900

    Python Study
    24/07/16

---
+++ 백준_브론즈/직각삼각형.py
@@ -0,0 +1,11 @@
+# 4153
+
+while True:
+    arr = list(map(int, input().split()))
+    if arr[0] == arr[1] == arr[2] == 0:
+        break
+    arr.sort()
+    if arr[0] ** 2 + arr[1] ** 2 == arr[2] ** 2:
+        print("right")
+    else:
+        print("wrong")
